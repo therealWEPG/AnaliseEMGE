14c786b431336a05c52c2385c7472fea7fb0a132
user <user@LAPTOP-MP36PSFE>
2021-11-30 22:34:17 +0000

    Adicionar arquivos de projeto.

---
+++ AnaliseEMGE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnaliseEMGE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
